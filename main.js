d612bf93adcc4b9d85f0ed2584a98534e034f918
Raphael Carloni Benzi <rbenzi@pop-os.localdomain>
2021-02-23 01:07:30 -0300

    adding code

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {Number} X 
+ * @param {Array} A 
+ */
+
+function solution (X, A) {
+    let arrPos = [];
+    let varPos = 0;
+    let sum = ((1 + X) * X) / 2;
+    for (let i = 0; i < A.length; i++) {
+
+        if (A[i] <= X && arrPos[A[i]] == undefined && arrPos[A[i]] !== A[i]) {
+			arrPos[A[i]] = A[i];
+            varPos += A[i];
+        }
+
+        if (sum == varPos) {
+            return i;
+        }
+
+    }
+
+    return -1;
+}
